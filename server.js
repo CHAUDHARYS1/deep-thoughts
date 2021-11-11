8e934fea20e213cc40af5e69277cc2e96cb7b670
Shital Chaudhary <shitalchau10@gmail.com>
2021-11-11 10:38:57 -0600

    Implement Auth Middleware to Populate Me Query

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const { authMiddleware } = require('./utils/auth');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 
@@ -17,7 +17,8 @@ const startServer = async () => {
   // create a new Apollo server and pass in our schema data
   const server = new ApolloServer({
     typeDefs, 
-    resolvers
+    resolvers,
+    context: authMiddleware
   });
 
   // start the Apollo server
