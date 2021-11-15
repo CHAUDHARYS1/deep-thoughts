7bb12ad45deb2c0f2a24e9e5e9e918956105bab7
Shital Chaudhary <shitalchau10@gmail.com>
2021-11-15 16:28:59 -0600

    Implement the Add Friend Button and Mutation

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,13 +4,24 @@ import { Redirect, useParams } from 'react-router-dom';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
 
-import { useQuery } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { ADD_FRIEND } from '../utils/mutations';
+import { useQuery, useMutation } from '@apollo/client';
 
 import Auth from '../utils/auth';
 
 
 const Profile = () => {
+  const [addFriend] = useMutation(ADD_FRIEND);
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   const { username: userParam } = useParams();
 
@@ -43,6 +54,12 @@ const Profile = () => {
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
+
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
